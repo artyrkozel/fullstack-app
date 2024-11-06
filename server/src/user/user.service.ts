477433ba3bef1a61d36613b8c1f178216d50e377
Artur Kozel <artyr_42@mail.ru>
2024-11-06 14:33:39 +0300

    user service added

---
+++ server/src/user/user.service.ts
@@ -0,0 +1,49 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { genSaltSync, hashSync } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    async create(createUserDto: CreateUserDto) {
+        const isUserExist = await this.prismaService.user.findFirst({ where: { email: createUserDto.email } });
+
+        if (isUserExist) {
+            throw new BadRequestException('User already exists');
+        }
+        const hashedPassword = this.hashPassword(createUserDto.password);
+
+        return await this.prismaService.user.create({
+            data: {
+                email: createUserDto.email,
+                password: hashedPassword,
+                roles: createUserDto.roles,
+            },
+        });
+    }
+
+    async findOne(idOrEmail: string) {
+        const user = await this.prismaService.user.findFirst({
+            where: {
+                OR: [{ id: idOrEmail }, { email: idOrEmail }],
+            },
+        });
+
+        if (!user) {
+            throw new BadRequestException('User not found');
+        }
+        return user;
+    }
+
+    remove(id: string) {
+        return this.prismaService.user.delete({
+            where: { id },
+        });
+    }
+
+    private hashPassword(password: string) {
+        return hashSync(password, genSaltSync(10));
+    }
+}
