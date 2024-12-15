5e6ea6f01ecf94d6289368a7de094115cfa1c1e1
Artur Kozel <artyr_42@mail.ru>
2024-12-15 11:43:35 +0300

    eslint config fix

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -15,7 +15,7 @@ export default [
             },
         },
         rules: {
-            // 'no-undef': 'off',
+            'no-undef': 'off',
             'no-unused-vars': 'off',
         },
         settings: {},
